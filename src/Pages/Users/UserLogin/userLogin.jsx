5e568d967bb32a2d1baceb2af1880a89d72b6874
AnandRPSICS <anandrp.sics@gmail.com>
2023-12-09 19:10:50 +0530

    users, orphnage and organizations login page Ui works completd

--- src/Pages/Users/UserLogin/userLogin.jsx
+++ src/Pages/Users/UserLogin/userLogin.jsx
@@ -1,35 +1,54 @@
 import { Container, Form, Button } from "react-bootstrap";
 import UserNavbar from "../../../Components/User/UserNavbar/userNavbar.jsx";
-import "./userLogin.css";
 import { useState } from "react";
 import UserLoginForm from "./userLoginForm.jsx";
 import OrganizationLogin from "../../Organizations/OrganizationLogin/OrganizationLogin.jsx";
 import AdminLoginForm from "../../Admin/AdminLogin/AdminLogin.jsx";
 import OrphanageLogin from "../../Orphanages/OrphanagesLogin/OrphanagesLogin.jsx";
-
+import CommunityHeader from "../../../Components/Common/CommunityHeader/CommunityHeader.jsx";
+import "./userLogin.css";
+import UserFooter from "../../../Components/Common/UserFooter/userFooter.jsx";
 const UserLogin = () => {
   const [activeUser, setActiveUser] = useState("user");
-
-  const handleChange = (e) => {
-    setActiveUser(e.target.value);
-  };
 
   return (
     <>
       <UserNavbar />
-      <Button variant="primary">Primary</Button>{" "}
-      <Container className="user-login-container">
-        <Form.Select onChange={handleChange}>
-          <option value="user">User</option>
-          <option value="orphanage">Orphanage</option>
-          <option value="organization">Organization</option>
-          <option value="admin">Admin</option>
-        </Form.Select>
+      <CommunityHeader />
+      <Container
+        id="all-login-form-container"
+        className="all-login-form-containers"
+      >
+        <Container className="user-login-btn-container">
+          <button
+            className={`${activeUser === "user" && "active"}`}
+            onClick={() => setActiveUser("user")}
+          >
+            User
+          </button>
+          <button
+            className={`${activeUser === "organization" && "active"}`}
+            onClick={() => setActiveUser("organization")}
+          >
+            {" "}
+            Organization
+          </button>
+          <button
+            className={`${activeUser === "orphanage" && "active"}`}
+            onClick={() => setActiveUser("orphanage")}
+          >
+            Orphanage
+          </button>
+        </Container>
+        <div>
+          {activeUser === "user" && <UserLoginForm user={"user"} />}
+          {activeUser === "orphanage" && <UserLoginForm user={"orphanage"} />}
+          {activeUser === "organization" && (
+            <UserLoginForm user={"organization"} />
+          )}
+        </div>
       </Container>
-      {activeUser === "user" && <UserLoginForm />}
-      {activeUser === "orphanage" && <OrphanageLogin />}
-      {activeUser === "organization" && <OrganizationLogin />}
-      {activeUser === "admin" && <AdminLoginForm />}
+      <UserFooter />
     </>
   );
 };
--- src/Pages/Users/UserLogin/userLoginForm.jsx
+++ src/Pages/Users/UserLogin/userLoginForm.jsx
@@ -1,8 +1,50 @@
-const UserLoginForm = () => {
+import { Form, Button, Col, InputGroup, Row } from "react-bootstrap";
+import { useState } from "react";
+const UserLoginForm = ({ user }) => {
+  console.log("user", user);
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
-    <>
-      <h1> User Login Form </h1>
-    </>
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Control
+          className="user-login-input"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <Form.Control.Feedback type="invalid">
+          Please provide a valid email.
+        </Form.Control.Feedback>
+
+        <Form.Control
+          className="user-login-input"
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <Form.Control.Feedback type="invalid">
+          Please provide a valid password
+        </Form.Control.Feedback>
+      </Form.Group>
+      <div className="user-login-btn-container-2">
+        <a href="#">Forgot Password?</a>
+        <br />
+
+        <Button className="user-login-btn" type="submit">
+          Login
+        </Button>
+      </div>
+    </Form>
   );
 };
 export default UserLoginForm;
