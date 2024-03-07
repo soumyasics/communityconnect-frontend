2696f776467627c8f85d3110e21e072e16027a03
Anand R P <anandrp.sics@gmail.com>
2024-03-07 13:00:35 +0530

    base url updated

--- src/api/Backend-url.js
+++ src/api/Backend-url.js
@@ -1,3 +1,2 @@
-// const BASE_URL = "http://hybrid.srishticampus.in/community_connect_tvm/backend";
-const BASE_URL = "http://localhost:5000";
-export default BASE_URL
+const BASE_URL = "http://hybrid.srishticampus.in:4001/";
+export default BASE_URL;
