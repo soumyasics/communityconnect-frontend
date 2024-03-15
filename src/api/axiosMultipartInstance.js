35c256886c40c1ed07eda19decc7c79b16722fa3
Anand R P <anandrp.sics@gmail.com>
2024-03-15 12:37:42 +0530

    url updated

--- src/api/axiosMultipartInstance.js
+++ src/api/axiosMultipartInstance.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const axiosMultipartInstance = axios.create({
-  baseURL: "http://hybrid.srishticampus.in/community_connect_tvm_api/" || "http://localhost:5000/",
-  headers: {
-    "Content-Type": "multipart/form-data",
-  },
-});
 // const axiosMultipartInstance = axios.create({
-//   baseURL:  "http://localhost:5000/",
+//   baseURL: "http://hybrid.srishticampus.in/community_connect_tvm_api/" || "http://localhost:5000/",
 //   headers: {
 //     "Content-Type": "multipart/form-data",
 //   },
 // });
+const axiosMultipartInstance = axios.create({
+  baseURL:  "http://localhost:5000/",
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+});
 export default axiosMultipartInstance;
